40b46d750c97308376c262ef27fdacc7fa1a17c6
darcy-buttrose <darcy.buttrose@gmail.com>
2015-05-18 19:00:27 +0800

    db - removed unwanted mock

--- test/ProjectedPayCalculator.mock.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Darcy on 16/05/2015.
- */
-export class ProjectedPayCalculator {
-    constructor(env) {
-        createSpy = env.createSpy;
-        calculate = createSpy('calculate');
-    }
-}
